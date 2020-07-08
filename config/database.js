ce36dfe0cbf7de26360249f68fa7aa07ac12d89f
emmanuelaka2010 <emmanuelaka2010@gmail.com>
2020-07-08 12:09:26 +0200

    ajout route, controllers

---
+++ config/database.js
@@ -0,0 +1,24 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+const mongoConnect = (callback) => {
+    MongoClient.connect('mongodb+srv://<username>:<password>@cluster0.jhooe.mongodb.net/eseg?retryWrites=true&w=majority')
+        .then(client => {
+            console.log('Connected !')
+            _db = client.db();
+            callback(client)
+        })
+        .catch(err => {
+            console.log(err);
+            throw err;
+        })
+}
+const getDb = () => {
+    if (_db) {
+        return _db
+    }
+    throw 'No Database found !'
+}
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
---
+++ models/index.js
@@ -0,0 +1,4 @@
+const dbConfig = require('../config/database')
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize();
